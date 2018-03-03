750f31aa08a31dc6add0217f01b53057f18769d3
Cédric JUNG <cedced19@gmail.com>
2018-03-03 17:42:11 +0100

    Generate and send a daily report

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ To get the address ip to identify the sensor you can do a request to **`/ip`**.
 To code the front javascript, you have to use: `npm run code`, it will start a browser-sync server.
 
 ![Demo](demo.png)
+
+## Mail config
+
+You will have to create a `mail-config.json` file to configurate the mail service:
+```json
+{
+    "language": "en",
+    "service": "gmail",
+    "auth": {
+        "user": "email",
+        "pass": "password"
+    }
+}
+```
+You can use a lot of service as described [there](http://nodemailer.com/smtp/well-known/).
+I use another GMail account to send me the data each day. 
--- tasks/send-weather-mail.js
+++ tasks/send-weather-mail.js
@@ -1,23 +1,28 @@
 
-var getExtremum = function (data, value) {
-    var lowest = Number.POSITIVE_INFINITY;
-    var highest = Number.NEGATIVE_INFINITY;
-    var tmp;
-    for (var i=data.length-1; i>=0; i--) {
-        tmp = data[i][value];
-        if (tmp < lowest) {
-            lowest = tmp;
-            lowestKey = i;
-        } 
-        if (tmp > highest) {
-            highest = tmp;
-            highestKey = i;
-        } 
-    }
-    return { highest: data[highestKey], lowest: data[lowestKey] };
+var mailConfig = [];
+var translation = {};
+try {
+    mailConfig = require('../mail-config.json');
+    translation = require('../i18n/' + mailConfig.language + '.json');
+} catch (e) {
+    throw e;
+    process.exit(1);
 }
 
+var getExtremums = require('get-extremums');
+var nodemailer = require('nodemailer');
+var moment = require('moment');
+moment.locale(mailConfig.language);
+
+var transporter = nodemailer.createTransport({
+    service: mailConfig.service,
+    auth: mailConfig.auth
+});
+
+
 module.exports = function (users, weatherData, sensors) {
+
+    // Attach weather data to sensors
     var list = [];
     sensors.forEach(function (el) {
         el.data = [];
@@ -28,7 +33,45 @@ module.exports = function (users, weatherData, sensors) {
         delete el.ip;
         sensors[list.indexOf(ip)].data.push(el);
     });
+
+    // Generate text
+    var text = `<h1>${translation['LAST_24_HOURS']}</h1><i>${translation['GENERATED_AT']}: ${moment().format('DD/MM/YY HH:mm')}</i>`;
     sensors.forEach(function (el) {
-        el.extremum = getExtremum(el.data, 'temperature');
+        el.extremum = getExtremums(el.data, 'temperature');
+        text += `<h2>${el.name}</h2>
+        <p><b>${translation['HOTTEST_PERIOD']}</b>
+        <i>${moment(new Date(el.extremum.highest.createdAt)).format('HH:mm')}</i>
+        <ul>
+            <li>${translation['TEMPERATURE']}: ${el.extremum.highest.temperature}°C</li>
+            <li>${translation['HEAT_INDEX']}: ${el.extremum.highest.heat_index}°C</li>
+            <li>${translation['HUMIDITY']}: ${el.extremum.highest.humidity}%</li>`;
+        if (el.extremum.highest.temperature2) {
+            text += `<li>${translation['TEMPERATURE']} n°2: ${el.extremum.highest.temperature2}°C</li>`
+        }
+        text += `</ul></p>
+        <p><b>${translation['COLDEST_PERIOD']}</b>
+        <i>${moment(new Date(el.extremum.lowest.createdAt)).format('HH:mm')}</i>
+        <ul>
+            <li>${translation['TEMPERATURE']}: ${el.extremum.lowest.temperature}°C</li>
+            <li>${translation['HEAT_INDEX']}: ${el.extremum.lowest.heat_index}°C</li>
+            <li>${translation['HUMIDITY']}: ${el.extremum.lowest.humidity}%</li>`;
+        if (el.extremum.lowest.temperature2) {
+            text += `<li>${translation['TEMPERATURE']} n°2: ${el.extremum.lowest.temperature2}°C</li>`
+        }
+        text += '</ul></p>';
+    });
+    
+    // Get receivers list
+    var receivers = [];
+    users.forEach(function (el) {
+        receivers.push(el.email);
+    });
+
+    // Send mail
+    transporter.sendMail({
+        from: `Sensorio <${mailConfig.auth.user}>`,
+        to: receivers.join(),
+        subject: 'Sensorio: ' + moment().format('DD MMMM YYYY'),
+        html: text
     });
 };
