fdd3a582277b4a8ebec270058724cd132ca074b3
Cédric JUNG <cedced19@gmail.com>
2019-04-07 18:50:00 +0200

    Show results

--- public/javascripts/controllers/fill-rate-cylinder-results.js
+++ public/javascripts/controllers/fill-rate-cylinder-results.js
@@ -1,3 +1,37 @@
-module.exports = ['$scope', '$http', '$rootScope', '$location', '$routeParams', '$translate', 'notie', function ($scope, $http, $rootScope, $location, $routeParams, $translate, notie) {
+module.exports = ['$scope', '$http', '$rootScope', '$routeParams', '$translate', 'notie', function ($scope, $http, $rootScope, $routeParams, $translate, notie) {
     
+    $scope.sensor = {};
+    $scope.calculate = function (el) {
+        el.V_empty = Number((Math.PI*el.radius*el.radius*(el.value-el.error)*1e-6).toFixed(3))
+        el.V_empty_L = el.V_empty*1000
+        el.V_tot = Number((Math.PI*el.radius*el.radius*el.height*1e-6).toFixed(3))
+        el.V_tot_L = el.V_tot*1000
+        el.V_full = Number((el.V_tot - el.V_empty).toFixed(3))
+        el.V_full_L = el.V_full*1000
+        el.percentage = Math.round(el.V_empty/el.V_tot*100);
+        console.log(el)
+        $scope.sensor = el
+    }
+    $scope.update = function () {
+        $http.get('/api/sensors/'+ $routeParams.ip).success(function(data) {
+            $scope.calculate(data);
+        }).error($rootScope.$error);
+    }
+    
+
+    if(Array.isArray($rootScope.sensors)){
+        var exists = false;
+        for(var k in $rootScope.sensors) {
+            if (k.ip == $routeParams.ip) {
+                exists = true;
+                $scope.calculate(k);
+                break;
+            }
+        }
+        if (!exists) {
+            $scope.update()
+        }
+    } else {
+        $scope.update()
+    }
 }];
